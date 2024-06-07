fdb84041e5cfec5461ea690d3a720a5c98f259d7
Max Unanian <Zeptorqyiad99@gmail.com>
2024-06-08 02:13:39 +0500

    Finished with Lesson_12 in course of JS

--- Lesson_12/main.js
+++ Lesson_12/main.js
@@ -1,20 +1,30 @@
-// Реализуйте в документе (в body) следующую структуру HTML-элементов с помощью встроенных функция создания элементов в Javascript:
+let counter = 0
+let container = document.createElement('div')
 
-/*
-<h1>Что я узнал в видео</h1>
-<ul>
-  <li>1) Как создавать HTML-элементы</li>
-  <li>2) Узнал, как добавлять текст и HTML-код в элемент</li>
-  <li>3) Как добавлять и удалять классы</li>
-  <li>4) Узнал, как вложить один элемент в другой</li>
-  <li>5) Узнал, что такое событие и обработчик события</li>
-  <li>6) Как назначить обработчик события</li>
-</ul>
-*/
+let title = document.createElement('h1')
+title.textContent = 'Что я узнал в видео'
 
-// Для создания элементов необходимо использовать:
-// document.createElement()
-// textContent
-// append()
+let list = document.createElement('ul')
+counter++
+let listItem1 = document.createElement('li')
+listItem1.textContent = `${counter}) Как создавать HTML-элементы`
+counter++
+let listItem2 = document.createElement('li')
+listItem2.textContent = `${counter}) Узнал, как добавлять текст и HTML-код в элемент`
+counter++
+let listItem3 = document.createElement('li')
+listItem3.textContent = `${counter}) Как добавлять и удалять классы`
+counter++
+let listItem4 = document.createElement('li')
+listItem4.textContent = `${counter}) Узнал, как вложить один элемент в другой`
+counter++
+let listItem5 = document.createElement('li')
+listItem5.textContent = `${counter}) Узнал, что такое событие и обработчик события`
+counter++
+let listItem6 = document.createElement('li')
+listItem6.textContent = `${counter}) Как назначить обработчик события`
 
-// document.write() мы уже не используем
+list.append(listItem1, listItem2, listItem3, listItem4, listItem5, listItem6)
+
+document.body.prepend(container)
+container.append(title, list)
