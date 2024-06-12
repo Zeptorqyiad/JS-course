760dcdd939840b47ed7662ee13ef25fea7fe000b
Max Unanian <Zeptorqyiad99@gmail.com>
2024-06-12 15:03:16 +0500

    Added new function in main.js

--- Lesson_17/main.js
+++ Lesson_17/main.js
@@ -1,5 +1,6 @@
 // Создание коробки
 let container = document.createElement('div')
+container.classList.add('container')
 let title = document.createElement('h1')
 title.textContent = 'Планер на день'
 title.classList.add('title__text')
@@ -20,16 +21,37 @@ button.textContent = 'Создать задачу'
 button.type = 'submit'
 
 // Функция возврата strong формата
-function getStrong(text) {
-   return `<strong>${text}</strong>`
-}
+// function getStrong(text) {
+//    return `<strong>${text}</strong>`
+// }
+
+let list = document.createElement('ul')
 
 // Функция получения данных
-function getWork() {
-	
+function getWork(task) {
+   // Добавление списка задач
+   let listItem = document.createElement('li')
+   let itemText = document.createElement('p')
+   itemText.classList.add('do__task')
+
+   // Вывод задачи
+   itemText.textContent = `${task}`
+
+   // Создание коробки list
+   list.append(listItem)
+   listItem.append(itemText)
+   return listItem
+}
+
+// Функция отправки задачи
+button.onclick = function () {
+   let task = input.value
+   let newTask = getWork(task)
+   list.append(newTask)
+   input.value = ''
 }
 
 // Вывод коробки
 document.body.prepend(container)
-container.append(title, blockInp)
+container.append(title, blockInp, list)
 blockInp.append(input, button)
