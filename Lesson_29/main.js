fcccdc71e58197eb316be30a3635a2ecfe964efa
Max Unanian <Zeptorqyiad99@gmail.com>
2024-06-19 19:21:12 +0500

    Minor fixed

--- Lesson_29/main.js
+++ Lesson_29/main.js
@@ -1,60 +1,57 @@
-// Это простое приложение для тестирования знаний. При клике на кнопку "Начать тестирование" запускается тест. В программе есть логическая ошибка. Почему-то всегда в результате тестирования выводится отрицательный исход: "Тест не пройден!". И один вопрос из трех не отображается в confirm(). Попробуйте исправить проблему.
-
 // Функция, возвращающая новую кнопку
 function getButton(text) {
-  let button = document.createElement("button")
-  button.textContent = text
-  button.classList.add("btn")
-  return button
+   let button = document.createElement('button')
+   button.textContent = text
+   button.classList.add('btn')
+   return button
 }
 
 // Запускает тестиование и возвращает результат типа Boolean
 function getTest() {
-  let resultFlag = true // Переменная флаг, который имзенится в случае неправильного ответа
+   let resultFlag = true // Переменная флаг, который имзенится в случае неправильного ответа
 
-  // Вопрос 1 
-  if (confirm("Boolean - числовой тип данных?") === true) {
-    resultFlag = false // Меняем значение флага
-  }
+   // Вопрос 1
+   if (confirm('Boolean - числовой тип данных?') === false) {
+      resultFlag = true // Меняем значение флага
+   }
 
-  // Вопрос 2
-  if (confirm("includes - возвращает кол-во символов в массиве?") === true) {
-  }
-  return false
+   // Вопрос 2
+   if (confirm('includes - возвращает кол-во символов в массиве?') === false) {
+      resultFlag = true // Меняем значение флага
+   }
 
-  // Вопрос 3
-  if (confirm("confirm - показывает окно выбора ДА / НЕТ") === false) {
-    resultFlag = false // Меняем значение флага
-  }
+   // Вопрос 3
+   if (confirm('confirm - показывает окно выбора ДА / НЕТ') === true) {
+      resultFlag = true // Меняем значение флага
+   }
 
-  return resultFlag
+   return resultFlag
 }
 
 // Главный заголовок
-let title = document.createElement("h1")
+let title = document.createElement('h1')
 title.textContent = `Тестирование на знание темы "Boolean"`
 
 // Кнопка для начала тестирования
-let startTestBtn = getButton("Начать тестирование")
+let startTestBtn = getButton('Начать тестирование')
 
 startTestBtn.onclick = function () {
+   // Блок для вывода результата тестирования
+   let resultBox = document.createElement('div')
 
-  // Блок для вывода результата тестирования
-  let resultBox = document.createElement("div")
+   // Запуск теста
+   let test = getTest()
 
-  // Запуск теста
-  let test = getTest()
+   if (test === true) {
+      resultBox.classList.add('result-success')
+      resultBox.textContent = 'Тест пройден успешно!'
+   } else {
+      resultBox.classList.add('result-error')
+      resultBox.textContent = 'Тест не пройден!'
+   }
 
-  if (test === true) {
-    resultBox.classList.add("result-success")
-    resultBox.textContent = "Тест пройден успешно!"
-  } else {
-    resultBox.classList.add("result-error")
-    resultBox.textContent = "Тест не пройден!"
-  }
-
-  startTestBtn.remove() // Удаление кнопки
-  document.body.append(resultBox)
+   startTestBtn.remove() // Удаление кнопки
+   document.body.append(resultBox)
 }
 
 document.body.append(title, startTestBtn)
