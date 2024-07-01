c9eee7e64e5431a65615a0d8bdd5c2d9898d1182
Max Unanian <Zeptorqyiad99@gmail.com>
2024-07-01 21:40:51 +0500

    Started final lesson in course of JS

---
+++ Lesson/Final_Lesson/js/main.js
@@ -0,0 +1,21 @@
+// Массив КАТАЛОГА товаров
+let catalogArr = [
+  {
+    title: "iPhone 14 Pro",
+    price: 110000,
+    desc: "Смартфон Apple iPhone 14 Pro 128GB",
+    img: 'img/1.jpg'
+  },
+  {
+    title: "AirPods Pro",
+    price: 2100,
+    desc: "Наушники Apple AirPods Pro (2-го поколения, 2022)",
+    img: 'img/2.jpg'
+  },
+  {
+    title: "Чехол iPhone 14 Pro",
+    price: 1200,
+    desc: "Чехол для Apple iPhone 14 Pro - желтый",
+    img: 'img/3.jpg'
+  }
+]
