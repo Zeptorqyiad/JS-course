ea26abad7c0af6299ca12e85f8c677452bd89fed
Max Unanian <Zeptorqyiad99@gmail.com>
2024-07-01 21:33:50 +0500

    Finished with Lesson_44

--- Lesson/Lesson_44/main.js
+++ Lesson/Lesson_44/main.js
@@ -1,5 +1,24 @@
 // Создание массива элементов
-let notesArr = []
+let notesArr = [
+   {
+      title: 'Заметка 1',
+      img: 'https://pic.rutubelist.ru/user/fd/76/fd766d07ff98aae9c6ef20cf3c5312d2.jpg',
+      desc: 'Описание 1',
+      done: false,
+   },
+   {
+      title: 'Заметка 2',
+      img: 'https://koshka.top/uploads/posts/2021-11/1638234774_67-koshka-top-p-britanskie-koti-polosatie-73.jpg',
+      desc: 'Описание 2',
+      done: true,
+   },
+   {
+      title: 'Заметка 3',
+      img: 'https://koshka.top/uploads/posts/2021-12/1638781024_66-koshka-top-p-izobrazhenie-kotikov-76.jpg',
+      desc: 'Описание 3',
+      done: false,
+   },
+]
 
 // Функция возрата кнопки
 function getAddBtn(text) {
@@ -9,7 +28,7 @@ function getAddBtn(text) {
 }
 
 // Функция возврата карточки
-function getCard(card) {
+function getCard(card, index) {
    let cardElement = document.createElement('li')
    let cardImage = document.createElement('img')
    let cardTitle = document.createElement('h2')
@@ -19,6 +38,11 @@ function getCard(card) {
 
    // Создание классов для объектов
    cardElement.classList.add('card')
+
+   if (card.done) {
+      cardElement.classList.add('card-important')
+   }
+
    cardImage.classList.add('card-image')
    cardTitle.classList.add('card-title')
    cardDesc.classList.add('card-desc')
@@ -33,6 +57,25 @@ function getCard(card) {
    // Описание кнопок
    cardRemoveBtn.textContent = 'Удалить'
    cardImportantBtn.textContent = 'Важное'
+
+   cardRemoveBtn.onclick = function () {
+      notesArr.splice(index, 1)
+      render(notesArr)
+   }
+
+   cardImportantBtn.onclick = function () {
+      if (!cardElement.classList.contains('card-important')) {
+         cardImportantBtn.textContent = 'Не важное'
+         cardElement.classList.add('card-important')
+         card.done = true
+      } else {
+         cardImportantBtn.textContent = 'Важное'
+         cardElement.classList.remove('card-important')
+         card.done = false
+      }
+
+      console.log(notesArr)
+   }
 
    // Добавление элементов в коробку
    cardElement.append(
@@ -69,6 +112,7 @@ addBtn.onclick = function () {
       title: titleValue,
       img: imageValue,
       desc: descValue,
+      done: false,
    }
 
    // Добавление элементов в массив
@@ -82,10 +126,12 @@ addBtn.onclick = function () {
 function render(arrNotes) {
    list.innerHTML = ''
    for (let i = 0; i < arrNotes.length; i++) {
-      let newCard = getCard(arrNotes[i])
+      let newCard = getCard(arrNotes[i], i)
       list.append(newCard)
    }
 }
+
+render(notesArr)
 
 // Создание общей коробки
 document.body.append(addBtn, list)
