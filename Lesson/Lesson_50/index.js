74415502933fb33ab71dec1b15ec152182c81173
Max Unanian <Zeptorqyiad99@gmail.com>
2024-08-18 00:49:17 +0500

    Finished with Lesson_51

--- Lesson/Lesson_50/index.js
+++ Lesson/Lesson_50/index.js
@@ -1,5 +1,21 @@
-if ('Яблоко' < 'Бана') {
-   console.log('Выполняется')
-} else {
-   console.log('Не выполняется')
+let inputEl = document.createElement('input')
+inputEl.type = 'text'
+
+let str = `JavaScript - это язык программирования, всё отлично работает в JavaScript`
+
+let textEl = document.createElement('p')
+textEl.textContent = str
+
+inputEl.oninput = function () {
+   let value = inputEl.value.trim()
+
+   if (value !== '') {
+      let highLightStr = `<span class="highlight">${value}</span>`
+
+      textEl.innerHTML = str.replaceAll(value, highLightStr)
+   } else {
+      textEl.textContent = str
+   }
 }
+
+document.body.append(inputEl, textEl)
