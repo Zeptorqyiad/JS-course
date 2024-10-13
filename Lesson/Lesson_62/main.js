41278a34b67211735f1f2c4dde3db14598aaf273
Max Unanian <Zeptorqyiad99@gmail.com>
2024-10-13 19:43:23 +0500

    Finished with Lesson_62

--- Lesson/Lesson_62/main.js
+++ Lesson/Lesson_62/main.js
@@ -1,2 +1,74 @@
 'use strict'
 
+// document.addEventListener('keydown', function (e) {
+//    console.log(e.key)
+//    console.log(e.code)
+// })
+
+// document.addEventListener('keyup', function (e) {
+//    console.log(e.key)
+//    console.log(e.code)
+// })
+
+// let inputEl = document.createElement('input')
+// inputEl.classList.add('__container')
+// inputEl.type = 'number'
+
+// inputEl.addEventListener('keydown', function (e) {
+//    if (e.key === '-') {
+//       e.preventDefault()
+//    }
+// })
+
+// document.body.append(inputEl)
+
+// let selectEl = document.createElement('select')
+
+// let option1El = document.createElement('option')
+// option1El.value = 'Красный'
+// option1El.textContent = 'Красный'
+
+// let option2El = document.createElement('option')
+// option2El.value = 'Синий'
+// option2El.textContent = 'Синий'
+
+// let option3El = document.createElement('option')
+// option3El.value = 'Зеленый'
+// option3El.textContent = 'Зеленый'
+
+// selectEl.append(option1El, option2El, option3El)
+
+// selectEl.addEventListener('change', function (e) {
+//    console.log(e.target.value)
+// })
+
+// let checkboxEl = document.createElement('input')
+// checkboxEl.type = 'checkbox'
+
+// checkboxEl.addEventListener('change', function (e) {
+//    console.log(e.target.checked)
+// })
+
+// let textareaEl = document.createElement('textarea')
+// textareaEl.style.resize = 'none'
+
+// textareaEl.addEventListener('change', function (e) {
+//    console.log(e.target.value)
+// })
+
+// let inputEl = document.createElement('input')
+// inputEl.type = 'text'
+
+// inputEl.addEventListener('change', function (e) {
+//    console.log(e.target.value)
+// })
+
+// inputEl.addEventListener('focus', function () {
+//    console.log('Элемент в фокусе')
+// })
+
+// inputEl.addEventListener('blur', function () {
+//    console.log('Элемент не в фокусе')
+// })
+
+// document.body.append(inputEl)
