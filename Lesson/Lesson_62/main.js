869d3807d06a999de314784cfe7700af9edcc393
Max Unanian <Zeptorqyiad99@gmail.com>
2024-10-13 21:56:03 +0500

    Finished with Lesson_63

--- Lesson/Lesson_62/main.js
+++ Lesson/Lesson_62/main.js
@@ -1,5 +1,3 @@
-'use strict'
-
 // document.addEventListener('keydown', function (e) {
 //    console.log(e.key)
 //    console.log(e.code)
--- Lesson/Lesson_63/main.js
+++ Lesson/Lesson_63/main.js
@@ -1,1 +1,26 @@
 'use strict'
+
+let boxEl = document.createElement('div')
+boxEl.classList.add('box')
+boxEl.textContent = ` мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама мама`
+
+boxEl.addEventListener('scroll', function (e) {
+   console.log(e.target.scrollTop)
+})
+
+let scrollTopBtnEl = document.createElement('button')
+scrollTopBtnEl.textContent = 'Вверх'
+
+scrollTopBtnEl.addEventListener('click', function () {
+   boxEl.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+   })
+})
+
+window.addEventListener('resize', function () {
+   console.log(window.innerWidth)
+   console.log(window.innerHeight)
+})
+
+document.body.append(boxEl, scrollTopBtnEl)
